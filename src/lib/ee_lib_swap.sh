683253711fd14155f3a0f32f87812969313fa8f9
Dan Newman <dan@clickbeyond.co.uk>
2015-02-11 15:05:15 +0000

    Only create a new EasyEngine swap file if it does not already exist

--- src/lib/ee_lib_swap.sh
+++ src/lib/ee_lib_swap.sh
@@ -2,30 +2,32 @@
 
 function ee_lib_swap()
 {
+EE_SWAPFILE=ee-swapfile
+if [ ! -f /${EE_SWAPFILE} ]; then
 	if [ $EE_TOTAL_RAM -le 8192 ]; then
 		if [ $EE_TOTAL_SWAP -eq 0 ];then
 			# Use fallocate command to create SWAP
 			# Swap Parameters:
 			# Size: Controlled by EE_SWAP (Dependant on RAM)
-			# Location: /ee-swapfile
+			# Location: /${EE_SWAPFILE}
 			ee_lib_echo "Adding swapfile, please wait..."
-			fallocate -l ${EE_SWAP}m /ee-swapfile &>> $EE_COMMAND_LOG \
-			|| ee_lib_error "Unable to generate /ee-swapfile, exit status = " $?
+			fallocate -l ${EE_SWAP}m /${EE_SWAPFILE} &>> $EE_COMMAND_LOG \
+			|| ee_lib_error "Unable to generate /${EE_SWAPFILE}, exit status = " $?
 
 			# Lock down access to the swap file
-			chmod 600 /ee-swapfile &>> $EE_COMMAND_LOG \
+			chmod 600 /${EE_SWAPFILE} &>> $EE_COMMAND_LOG \
 			|| ee_lib_error "Unable to lock down access to swapfile, exit status = " $?
 
 			# Create it as a Swap
-			mkswap /ee-swapfile &>> $EE_COMMAND_LOG \
+			mkswap /${EE_SWAPFILE} &>> $EE_COMMAND_LOG \
 			|| ee_lib_error "Unable to create swapfile, exit status = " $?
 
 			# Enable the Swap
-			swapon /ee-swapfile &>> $EE_COMMAND_LOG \
+			swapon /${EE_SWAPFILE} &>> $EE_COMMAND_LOG \
 			|| ee_lib_error "Unable to enable Swap, exit status = " $?
 
 			# Add entry into /etc/fstab
-			echo "/ee-swapfile		none		swap	sw	0	0" >> /etc/fstab \
+			echo "/${EE_SWAPFILE}		none		swap	sw	0	0" >> /etc/fstab \
 			|| ee_lib_error "Unable to add entry into /etc/fstab, exit status = " $?
 
 			# Tweak the swap settings by adding entries into /etc/sysctl.conf
@@ -41,4 +43,5 @@ function ee_lib_swap()
 			|| ee_lib_error "Unable to add entry into /etc/sysctl.conf, exit status = " $?
 		fi
 	fi
+fi
 }
