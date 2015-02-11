854e6140f92fbedb3294f8b56b6eeb6d58e03a75
Dan Newman <dan@clickbeyond.co.uk>
2015-02-11 09:23:08 +0000

    Automatically create a swap file for all systems with 8192MB of RAM or less
    The size of the swap file created is relative to the available RAM, as recommended by this EasyEngine article: https://rtcamp.com/easyengine/installation/

--- src/lib/ee_lib_swap.sh
+++ src/lib/ee_lib_swap.sh
@@ -2,28 +2,44 @@
 
 function ee_lib_swap()
 {
-	if [ $EE_TOTAL_RAM -le 512 ]; then
+	if [ $EE_TOTAL_RAM -le 8192 ]; then
 		if [ $EE_TOTAL_SWAP -le $EE_SWAP ];then
 
-			# Use dd command to create SWAP
+			# Use fallocate command to create SWAP
 			# Swap Parameters:
+			# Size: Controlled by EE_SWAP (Dependant on RAM)
 			# Location: /ee-swapfile
-			# Block Size: 1024
-			ee_lib_echo "Adding 1GB swapfile, please wait..."
-			dd if=/dev/zero of=/ee-swapfile bs=1024 count=1048k &>> $EE_COMMAND_LOG \
+			ee_lib_echo "Adding swapfile, please wait..."
+			fallocate -l ${EE_SWAP}m /ee-swapfile &>> $EE_COMMAND_LOG \
 			|| ee_lib_error "Unable to generate /ee-swapfile, exit status = " $?
+
+			# Lock down access to the swap file
+			chmod 600 /ee-swapfile &>> $EE_COMMAND_LOG \
+			|| ee_lib_error "Unable to lock down access to swapfile, exit status = " $?
 
 			# Create it as a Swap
 			mkswap /ee-swapfile &>> $EE_COMMAND_LOG \
 			|| ee_lib_error "Unable to create swapfile, exit status = " $?
 
-			# On the Swap
+			# Enable the Swap
 			swapon /ee-swapfile &>> $EE_COMMAND_LOG \
-			|| ee_lib_error "Unable to on Swap, exit status = " $?
+			|| ee_lib_error "Unable to enable Swap, exit status = " $?
 
 			# Add entry into /etc/fstab
 			echo "/ee-swapfile		none		swap	sw	0	0" >> /etc/fstab \
 			|| ee_lib_error "Unable to add entry into /etc/fstab, exit status = " $?
+
+			# Tweak the swap settings by adding entries into /etc/sysctl.conf
+			# Default swappiness = 60
+			sysctl vm.swappiness=10 &>> $EE_COMMAND_LOG \
+			|| ee_lib_error "Unable to set swappiness, exit status = " $?
+			echo "vm.swappiness=10" >> /etc/sysctl.conf \
+			|| ee_lib_error "Unable to add entry into /etc/sysctl.conf, exit status = " $?
+			# Default cache_pressure = 100
+			sysctl vm.vfs_cache_pressure=50 &>> $EE_COMMAND_LOG \
+			|| ee_lib_error "Unable to set vfs_cache_pressure, exit status = " $?
+			echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.conf \
+			|| ee_lib_error "Unable to add entry into /etc/sysctl.conf, exit status = " $?
 		fi
 	fi
 }
