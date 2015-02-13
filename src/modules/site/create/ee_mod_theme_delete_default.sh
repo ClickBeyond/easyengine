6e3a671cc1bba738126522150f54d8f5ac9e19ce
Dan Newman <dan@clickbeyond.co.uk>
2015-02-13 15:27:16 +0000

    Fix syntax error to delete Twenty Thirteen theme

--- src/modules/site/create/ee_mod_theme_delete_default.sh
+++ src/modules/site/create/ee_mod_theme_delete_default.sh
@@ -3,12 +3,12 @@
 function ee_mod_theme_delete_default()
 {
 	cd /var/www/$EE_DOMAIN/htdocs/
-	ee_lib_echo "Deleting default theme, please wait..."
+	ee_lib_echo "Deleting default themes, please wait..."
 	# Delete Twenty Fourteen
 	wp theme --allow-root delete twentyfourteen &>> $EE_COMMAND_LOG \
 	|| ee_lib_error "Unable to delete Twenty Fourteen theme, exit status = " $?
 	
 	# Delete Twenty Thirteen
-	wp plugin --allow-root delete twentythirteen &>> $EE_COMMAND_LOG \
+	wp theme --allow-root delete twentythirteen &>> $EE_COMMAND_LOG \
 	|| ee_lib_error "Unable to delete Twenty Thirteen plugin, exit status = " $?
 }
